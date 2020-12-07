e5127e40218edb72e2f468abd7c8307ae9a6e451
Stergios Tziahanas <stzia@yahoo.com>
2020-12-07 21:52:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
-$(document).ready(function(){
-    $("#calculate").click(function(){
-        var mdate = $("#birth_date").val().toString();
-        var yearThen = parseInt(mdate.substring(0,4), 10);
-        var monthThen = parseInt(mdate.substring(5,7), 10);
-        var dayThen = parseInt(mdate.substring(8,10), 10);
-        
-        var today = new Date();
-        var birthday = new Date(yearThen, monthThen-1, dayThen);
-        
-        var differenceInMilisecond = today.valueOf() - birthday.valueOf();
-        
-        var year_age = Math.floor(differenceInMilisecond / 31536000000);
-        var day_age = Math.floor((differenceInMilisecond % 31536000000) / 86400000);
-        
-        if ((today.getMonth() == birthday.getMonth()) && (today.getDate() == birthday.getDate())) {
-            alert("Happy B'day!!!");
-        }
-        
-        var month_age = Math.floor(day_age/30);
-        
-        day_age = day_age % 30;
-        
-        if (isNaN(year_age) || isNaN(month_age) || isNaN(day_age)) {
-            $("#exact_age").text("Invalid birthday - Please try again!");
-        }
-        else {
-            $("#exact_age").html("You are<br/><span id=\"age\">" + year_age + " years " + month_age + " months " + day_age + " days</span> old");
-        }
-    });
-});
+var showYears = document.getElementById('showYears');
+var showMonths = document.getElementById('showMonths');
+var showDays = document.getElementById('showDays');
+var button = document.getElementById('btn');
+
+btn.onclick = function () {
+        var dateStart = document.getElementById('dateStart').value;
+        var dateEnd = document.getElementById('dateEnd').value;
+        var dateStarting = new Date(dateStart);
+        var dateEnding = new Date(dateEnd);
+        var diff = dateEnding - dateStarting;
+        var years = Math.floor(diff / (365.25 * 24 * 60 * 60 * 1000));
+        var remaining = Math.floor(diff % (365.25 * 24 * 60 * 60 * 1000));
+        var months = Math.floor(remaining / ((365.25 / 12) * 24 * 60 * 60 * 1000));
+        var remaining = Math.floor(remaining % ((365.25 / 12) * 24 * 60 * 60 * 1000));
+        var days = Math.floor(remaining / (24 * 60 * 60 * 1000));
+        showYears.innerHTML = years;
+        showMonths.innerHTML = months;
+        showDays.innerHTML = days;
+}
